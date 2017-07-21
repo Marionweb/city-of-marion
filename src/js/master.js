da7bdcb2cb9a62806ae075bd2e4364a9e7ebe9b0
Mike Stecker <mike@stecker.me>
2017-07-21 16:30:35 -0700

    header mostly complete

--- src/js/master.js
+++ src/js/master.js
@@ -1,19 +1,40 @@
 docReady(function() {
-  var mainMenuOpenBtn = document.getElementById('menu--side--toggle-open'),
-   mainMenuCloseBtn = document.getElementById('menu--side--toggle-close'),
+  var mainMenuToggleBread = document.getElementsByClassName('menu--side--toggle'),
+   mainMenuOpenAnchor = document.getElementsByClassName('menu--side--toggle-open'),
+   mainMenuCloseAnchor = document.getElementsByClassName('menu--side--toggle-close'),
    sideMenuCloseBtn = document.getElementById('menu--side--close'),
    container = document.querySelector('#container');
 
-  mainMenuOpenBtn.addEventListener("click", toggleNav);
-  mainMenuCloseBtn.addEventListener("click", toggleNav);
+  for (var i = 0; i < mainMenuToggleBread.length; i++) {
+    mainMenuToggleBread[i].addEventListener('click', toggleNav, false);
+  }
+  for (var i = 0; i < mainMenuOpenAnchor.length; i++) {
+    mainMenuOpenAnchor[i].addEventListener('click', toggleNav, false);
+  }
+  for (var i = 0; i < mainMenuCloseAnchor.length; i++) {
+    mainMenuCloseAnchor[i].addEventListener('click', toggleNav, false);
+  }
+
   sideMenuCloseBtn.addEventListener("click", toggleNav);
 
   function toggleNav(event) {
     if(!container.hasClass('menu--open')) {
       container.classList.add('menu--open');
+      for (var i = 0; i < mainMenuOpenAnchor.length; i++) {
+        mainMenuOpenAnchor[i].style.display = 'none';
+      }
+      for (var i = 0; i < mainMenuCloseAnchor.length; i++) {
+        mainMenuCloseAnchor[i].style.display = 'block';
+      }
       event.preventDefault();
     } else {
       container.classList.remove('menu--open');
+      for (var i = 0; i < mainMenuOpenAnchor.length; i++) {
+        mainMenuOpenAnchor[i].style.display = 'block';
+      }
+      for (var i = 0; i < mainMenuCloseAnchor.length; i++) {
+        mainMenuCloseAnchor[i].style.display = 'none';
+      }
       event.preventDefault();
     }
   }
