a1d099d7f5ab0b72418275e48d495f5db9ebeb26
Mike Stecker <mike@stecker.me>
2017-09-17 17:40:31 -0700

    started directory contact modal

---
+++ config/contact-form.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contact Form Settings
+ */
+
+// $_ENV constants are loaded by craft3-multi-environment from .env.php via web/index.php
+return [
+
+    // All environments
+    '*' => [
+      'toEmail' => Craft::$app->request->getValidatedBodyParam('toEmail'),
+      'prependSubject'      => 'New message from City of Marion Website',
+      'prependSender'       => 'On behalf of',
+      'allowAttachments'    => false,
+      'successFlashMessage' => 'Your message has been sent.'
+    ],
+];
