33a0acabaaae8b2ab424897a7b0c2237533a5c2d
Mike Stecker <mike@stecker.me>
2017-11-07 17:38:22 -0800

    added bugsnag, update composer

---
+++ config/app.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Boostrap
+ */
+
+// $_ENV constants are loaded by craft3-multi-environment from .env.php via web/index.php
+return [
+
+    // All environments
+    '*' => [
+    ],
+
+    // Live (production) environment
+    'live'  => [
+    ],
+
+    // Staging (pre-production) environment
+    'staging'  => [
+    ],
+
+    // Local (development) environment
+    'local'  => [
+      'bootstrap' => [ '\pinfirestudios\craft3bugsnag\Bootstrap' ]
+      // $bugsnag = Bugsnag\Client::make("d5ca17b5fb346c7877b18e79f0f3c49a");
+    ],
+];
---
+++ config/craft3-bugsnag.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * craft3-bugsnag plugin for Craft CMS 3.x
+ *
+ * Enables errors to be sent to Bugsnag (www.bugsnag.com)
+ *
+ * @link      https://www.pinfirestudios.com
+ * @copyright Copyright (c) 2017 Pinfire Studios <www.pinfirestudios.com>
+ */
+/**
+ * craft3-bugsnag config.php
+ *
+ * This file exists only as a template for the craft3-bugsnag settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'craft3-bugsnag.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+return [
+  'apiKey' => getenv('CRAFTENV_BUGSNAG_API_KEY'),
+  // Defaults to CRAFT_ENVIRONMENT if null
+  'releaseStage' => getenv('CRAFTENV_CRAFT_ENVIRONMENT'),
+  // 'notifyReleaseStages' => ['staging', 'production'],
+];
