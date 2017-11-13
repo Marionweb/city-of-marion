c682ba7d42ef887aa0f52e513f7b8b202ec8d2f5
Mike Stecker <mike@stecker.me>
2017-11-12 17:16:30 -0800

    stashing for later

---
+++ config/pdf-factory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * PDF Factory plugin for Craft CMS 3.x
+ *
+ * Plugin to generate images from PDF's. Possibly adding future functionalities?
+ *
+ * @link      http://github.com/mikestecker
+ * @copyright Copyright (c) 2017 Mike Stecker
+ */
+
+/**
+ * PDF Factory config.php
+ *
+ * This file exists only as a template for the PDF Factory settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'pdf-factory.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+
+    // This is the location where your images would be stored after generated.
+    'cachePath' => 'generated/',
+
+];
