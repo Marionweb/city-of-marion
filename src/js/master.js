1cea328ff0fb08a67a6ed0ecf35305d16f90c21b
Mike Stecker <mike@stecker.me>
2019-03-20 10:01:25 -0700

    Added outdated browser message

--- src/js/master.js
+++ src/js/master.js
@@ -1,5 +1,30 @@
 'use strict';
 
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+      window.onload = func;
+  } else {
+      window.onload = function() {
+          if (oldonload) {
+              oldonload();
+          }
+          func();
+      }
+  }
+}
+//call plugin function after DOM ready
+addLoadEvent(function(){
+  outdatedBrowser({
+      bgColor: '#f25648',
+      color: '#ffffff',
+      lowerThan: 'Edge',
+      languagePath: '/outdatedbrowser/lang/en.html'
+  })
+});
+
+
+
 docReady(function() {
 
   // SLIDE-OUT NAVIGATION
