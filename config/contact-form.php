16ccdd5c0ac9ace3905fb45a6497fecd2fd1f685
Mike Stecker <mike@stecker.me>
2020-08-05 15:26:05 -0700

    craft update

--- config/contact-form.php
+++ config/contact-form.php
@@ -4,15 +4,21 @@
  * Contact Form Settings
  */
 
-// $_ENV constants are loaded by craft3-multi-environment from .env.php via web/index.php
-    return [
+$config = [
+  '*' => [
+    'prependSubject'      => 'New message from City of Marion Website',
+    'prependSender'       => 'On behalf of',
+    'allowAttachments'    => false,
+    'successFlashMessage' => 'Message sent!'
+  ],
+];
+$request = Craft::$app->request;
 
-      // All environments
-      '*' => [
-        'toEmail'             => Craft::$app->request->getValidatedBodyParam('toEmail'),
-        'prependSubject'      => 'New message from City of Marion Website',
-        'prependSender'       => 'On behalf of',
-        'allowAttachments'    => false,
-        'successFlashMessage' => 'Message sent!'
-      ],
-    ];
+if (
+    !$request->getIsConsoleRequest() &&
+    ($toEmail = $request->getValidatedBodyParam('toEmail')) !== null
+) {
+    $config['toEmail'] = $toEmail;
+}
+
+return $config;
